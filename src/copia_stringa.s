4cd0de9b6c8edd75ab4e3efb1137333d3ae738c3
klaudioMema <klaudiomema2000@gmail.com>
2022-06-29 15:24:26 +0200

    modifica parametri

--- src/copia_stringa.s
+++ src/copia_stringa.s
@@ -1,5 +1,10 @@
 # funzione che copia una stringa da un indirizzo sorgente ad uno destinazione fino al carattere di terminazione "\0"
-# l'indirizzo sorgente deve essere contenuto in esi mentre la destinazione di edi
+# l'indirizzo sorgente deve essere contenuto in EAX mentre la destinazione in EDI
+#
+# dato che bisogna copiare anche stringhe dallo stack, in questo caso l'indirizzo
+# va decrementato. Quindi nel registro EBX mettiamo un altro parametro che indica
+# come prelevare la stringa dall'indirizzo EAX, se vale 0 bisogna incrementare,
+# altrimenti bisogna decrementare.
 
 .section .data
 .section .text
@@ -7,22 +12,32 @@
 .type copia_stringa, @function
 copia_stringa:
 
-push %eax
+push %ecx  					#salvo ecx perchè vado a modificarlo in seguito
 
 ciclo:
-cmp $0, (%esi)
+cmp $0, (%eax)
 jz fine_funzione
 
-movb (%esi), %ah
-movb %ah, (%edi)
-inc %esi
-inc %edi
+movb (%eax), %cl
+movb %cl, (%edi)
 
+cmpl $0, %ebx
+jz incrementa
+
+#decrementa
+dec %eax
+jmp continua
+
+incrementa:
+inc %eax
+
+continua:
+inc %edi
 jmp ciclo
 
 
 fine_funzione:
 
-pop %eax
+pop %ecx
 
 ret
